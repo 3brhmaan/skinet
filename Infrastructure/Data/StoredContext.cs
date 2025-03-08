4ba5811943c3ad1a8ae82b0c873e891b30013841
abdullrhman <3brhmaaan@gmail.com>
2025-03-08 16:41:06 +0200

    Add Product entity configuration and initial migration
    - Added ProductConfiguration class to configure Product entity.
    - Modified StoredContext to apply ProductConfiguration.
    - Added initial migration files for Product entity.

--- Infrastructure/Data/StoredContext.cs
+++ Infrastructure/Data/StoredContext.cs
@@ -1,8 +1,15 @@
 ﻿using Core.Entities;
+using Infrastructure.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 public class StoredContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
