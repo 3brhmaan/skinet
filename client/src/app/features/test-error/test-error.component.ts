8a77e071a01482187dd6ab28f9ee2d82bcbcd9d7
abdullrhman <3brhmaaan@gmail.com>
2025-03-29 17:08:07 +0200

    Enhance error handling for validation in interceptor
    - Updated the `errorInterceptor` to better manage 400 status validation errors by collecting and throwing them as a flat array.
    - Added a new section in `test-error.component.html` to display these validation errors.
    - Introduced a `validationErrors` property in `test-error.component.ts` to store error messages and updated the `get400ValidationError` method to populate this property.

--- client/src/app/features/test-error/test-error.component.ts
+++ client/src/app/features/test-error/test-error.component.ts
@@ -11,6 +11,7 @@ import { MatButton } from '@angular/material/button';
 export class TestErrorComponent {
   private http = inject(HttpClient);
   baseUrl = 'https://localhost:5001/api/';
+  validationErrors?: string[];
 
   get400Error() {
     this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
@@ -22,7 +23,7 @@ export class TestErrorComponent {
   get400ValidationError() {
     this.http.post(this.baseUrl + 'buggy/validationerror', {}).subscribe({
       next: (response) => console.log(response),
-      error: (error) => console.log(error),
+      error: (error) => (this.validationErrors = error),
     });
   }
 
