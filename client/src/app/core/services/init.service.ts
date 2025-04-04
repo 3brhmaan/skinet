80410d80171ce4607fb90b97f45f547bc3cd39e5
abdullrhman <3brhmaaan@gmail.com>
2025-04-04 02:14:25 +0200

    persist cart data

--- client/src/app/core/services/init.service.ts
+++ client/src/app/core/services/init.service.ts
@@ -1,16 +1,22 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account.service';
-import { forkJoin } from 'rxjs';
+import { forkJoin, of } from 'rxjs';
+import { CartService } from './cart.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InitService {
   private accountService = inject(AccountService);
+  private cartService = inject(CartService);
 
   init() {
+    const cartId = localStorage.getItem('cart_id');
+    const cart$ = cartId ? this.cartService.getCart(cartId) : of(null);
+
     return forkJoin({
       user: this.accountService.getUserInfo(),
+      cart: cart$,
     });
   }
 }
