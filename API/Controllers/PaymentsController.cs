abaca3cff1ccfed929c6f9b961325c8c5f22068a
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2025-04-09 01:12:57 +0200

    Add payment processing functionality and Stripe integration
    - Created PaymentsController for payment API endpoints.
    - add Stripe settings to appsettings.json.
    - Enhanced ShoppingCart model with payment-related properties.
    - Introduced IPaymentService interface and implemented PaymentService for handling payment intents with Stripe.
    - Updated Infrastructure.csproj to include Stripe.net package.

---
+++ API/Controllers/PaymentsController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/Payments")]
+[ApiController]
+public class PaymentsController(
+    IPaymentService paymentService ,
+    IGenericRepository<DeliveryMethod> dmRepository
+)
+    : ControllerBase
+{
+    [HttpPost("{cartId}")]
+    [Authorize]
+    public async Task<IActionResult> CreateOrUpdatePaymentIntent(string cartId)
+    {
+        var cart = await paymentService.CreateOrUpdatePaymentIntent(cartId);
+
+        if (cart is null)
+        {
+            return BadRequest("Problem with your cart");
+        }
+
+        return Ok(cart);
+    }
+
+    [HttpGet("delivery-methods")]
+    public async Task<IActionResult> GetDeliveryMethod()
+    {
+        return Ok(await dmRepository.GetAllAsync());
+    }
+
+}
--- Core/Entities/ShoppingCart.cs
+++ Core/Entities/ShoppingCart.cs
@@ -9,4 +9,7 @@ public class ShoppingCart
 {
     public string Id { get; set; }
     public List<CartItem> Items { get; set; } = new List<CartItem>();
+    public int? DeliveryMethodId { get; set; }
+    public string? ClientSecret { get; set; }
+    public string? PaymentIntentId { get; set; }
 }
