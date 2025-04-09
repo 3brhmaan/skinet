e32d86d89a7fac8fb8307722be1ee1ae9fe5109b
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2025-04-09 05:07:07 +0200

    Fix typos in DeliveryMethod properties and migrations
    Corrected property names `ShortNamr` and `DeleviryTime` to `ShortName` and `DeliveryTime` in the `DeliveryMethod` class. Updated the migration file `20250409030543_fixDeliveryMethodColumnsTypo.cs` and the model snapshot `StoredContextModelSnapshot.cs` to reflect these changes, ensuring the database columns are renamed accordingly.

--- Core/Entities/DeliveryMethod.cs
+++ Core/Entities/DeliveryMethod.cs
@@ -2,8 +2,8 @@
 
 public class DeliveryMethod: BaseEntity
 {
-    public required string ShortNamr { get; set; }
-    public required string DeleviryTime { get; set; }
+    public required string ShortName { get; set; }
+    public required string DeliveryTime { get; set; }
     public required string Description { get; set; }
     public decimal Price { get; set; }
 }
---
+++ Infrastructure/Migrations/20250409030543_fixDeliveryMethodColumnsTypo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixDeliveryMethodColumnsTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShortNamr",
+                table: "DeliveryMethods",
+                newName: "ShortName");
+
+            migrationBuilder.RenameColumn(
+                name: "DeleviryTime",
+                table: "DeliveryMethods",
+                newName: "DeliveryTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShortName",
+                table: "DeliveryMethods",
+                newName: "ShortNamr");
+
+            migrationBuilder.RenameColumn(
+                name: "DeliveryTime",
+                table: "DeliveryMethods",
+                newName: "DeleviryTime");
+        }
+    }
+}
