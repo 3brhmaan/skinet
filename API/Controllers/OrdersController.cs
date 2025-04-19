8aa11f223008e1dda753b9805c4d183be184ec45
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2025-04-19 20:11:51 +0200

    Implement order creation and update database schema
    - Added `OrdersController` with `CreateOrder` method to handle order processing.
    - Updated `OrderForCreationDto` to include required properties for order creation.
    - Renamed `Year` to `ExpYear` in `PaymentSummary` for consistency.
    - Generated migration files to update database schema, including renaming columns and managing role data.
    - Updated model snapshot to reflect the current database structure.

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,76 @@
+﻿using API.DTOs;
+using API.Extensions;
+using Core.Entities;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Authorize]
+public class OrdersController(
+    ICartService cartService,
+    IUnitOfWork unitOfWork
+) : BaseApiController
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder(OrderForCreationDto orderDto)
+    {
+        var email = User.GetEmail();
+
+        var cart = await cartService.GetCartAsync(orderDto.CartId);
+
+        if (cart == null) 
+            return BadRequest("Cart Not Found");
+
+        if(cart.PaymentIntentId == null)
+            return BadRequest("No payment inent for this order");
+            
+        var items = new List<OrderItem>();
+        foreach(var item in cart.Items)
+        {
+            var product = await unitOfWork.Repository<Product>().GetByIdAsync(item.ProductId);
+
+            if(product == null)
+                return BadRequest("Problem with order");
+
+            var itemOrdered = new ProductItemOrderd
+            {
+                ProductId = item.ProductId,
+                ProductName = item.ProductName,
+                PictureUrl = item.PictureUrl,
+            };
+
+            var orderItem = new OrderItem
+            {
+                ItemOrderd = itemOrdered,
+                Price = product.Price,
+                Quantity = item.Quantity
+            };
+
+            items.Add(orderItem);
+        }
+
+        var deliverMethod = await unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(orderDto.DeliveryMethodId);
+        if(deliverMethod == null)
+            return BadRequest("No delivery method selected");
+
+        var order = new Order
+        {
+            OrderItems = items,
+            DeliveryMethod = deliverMethod,
+            ShippingAddress = orderDto.ShippingAddress,
+            SubTotal = items.Sum(x => x.Price * x.Quantity),
+            PaymentSummary = orderDto.PaymentSummary,
+            PaymentIntentId = cart.PaymentIntentId,
+            BuyerEmail = email
+        };
+
+        unitOfWork.Repository<Order>().Add(order);
+        if(await unitOfWork.Complete())
+            return Ok(order);
+
+        return BadRequest("Problem Creating order.");
+    }
+}
---
+++ Infrastructure/Migrations/20250419180921_PaymentSummaryCorrection.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentSummaryCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0f4fba4f-f61e-49e6-af33-d524c0142cbb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8b899221-1b80-4ffa-8f7f-c47a34f5a52c");
+
+            migrationBuilder.RenameColumn(
+                name: "PaymentSummary_Year",
+                table: "Orders",
+                newName: "PaymentSummary_ExpYear");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "97e326b1-7727-4012-bfd1-fdf40628faf7", null, "Admin", "ADMIN" },
+                    { "c3dfb58c-0c08-411b-ba81-e5c8d31df048", null, "Customer", "CUSTOMER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "97e326b1-7727-4012-bfd1-fdf40628faf7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c3dfb58c-0c08-411b-ba81-e5c8d31df048");
+
+            migrationBuilder.RenameColumn(
+                name: "PaymentSummary_ExpYear",
+                table: "Orders",
+                newName: "PaymentSummary_Year");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0f4fba4f-f61e-49e6-af33-d524c0142cbb", null, "Customer", "CUSTOMER" },
+                    { "8b899221-1b80-4ffa-8f7f-c47a34f5a52c", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
